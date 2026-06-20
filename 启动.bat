e17a9d270363bcc3354e6e96d717e6af7728ac50
dcr_coof <3492319570@qq.com>
2026-06-20 09:21:35 +0800

    chore: update launch script, remove .hermes/desktop-attachments

--- 启动.bat
+++ 启动.bat
@@ -1,2 +1,8 @@
 @echo off
-start javaw -jar "%~dp0target\zuul-1.0-SNAPSHOT-jar-with-dependencies.jar"
+cd /d "%~dp0"
+if not exist "target\zuul-1.0-SNAPSHOT-jar-with-dependencies.jar" (
+    echo 正在打包，首次需要下载依赖，请稍候...
+    call mvn package -DskipTests -q
+)
+echo 启动 Chronicle of the Lost Realms...
+start javaw -jar "target\zuul-1.0-SNAPSHOT-jar-with-dependencies.jar"
